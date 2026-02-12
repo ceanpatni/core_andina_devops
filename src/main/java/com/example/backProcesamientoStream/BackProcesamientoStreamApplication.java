221f3bb43b251d7a725a02c40b1785229e8f5a36
Jhon Silva <jhon.silva@linktic.com>
2026-02-12 16:20:38 -0500

    update properties

--- src/main/java/com/example/backProcesamientoStream/BackProcesamientoStreamApplication.java
+++ src/main/java/com/example/backProcesamientoStream/BackProcesamientoStreamApplication.java
@@ -1,5 +1,6 @@
 package com.example.backProcesamientoStream;
 
+import com.example.backProcesamientoStream.config.PropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling  // habilita @Scheduled
 @EnableAsync       // habilita ejecución asíncrona
-@EnableConfigurationProperties
+@EnableConfigurationProperties(PropertiesConfig.class)
 public class BackProcesamientoStreamApplication {
 
 	public static void main(String[] args) {
